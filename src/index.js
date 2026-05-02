5e626c55ab5401832cc440bc00bee0041bfd4da7
vijeyadoreindia-cell <vijey.adoreindia@gmail.com>
2026-05-02 21:31:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Hide the HTML loading screen once React has rendered
+// requestAnimationFrame ensures the first paint has happened
+requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
+    if (typeof window.__hideLoader === "function") {
+      window.__hideLoader();
+    }
+  });
+});
