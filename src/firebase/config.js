d559c94c3a4cd4f1276a24cf1cd109796920b74f
vijeyadoreindia-cell <vijey.adoreindia@gmail.com>
2026-05-03 06:09:22 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
@@ -17,7 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);          // ← added for image uploads
 export const googleProvider = new GoogleAuthProvider();
 export const analytics = getAnalytics(app);
 export default app;
